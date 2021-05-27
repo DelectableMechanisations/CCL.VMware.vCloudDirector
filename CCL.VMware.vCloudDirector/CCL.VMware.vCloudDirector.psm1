a8220d3e2720a669e64595dfdebe57b01b485da2
David Baumbach <David.Baumbach@outlook.com>
2021-05-27 18:37:52 +1200

    Update scaffolding scripts to Stucco version 0.4.0

--- CCL.VMware.vCloudDirector/CCL.VMware.vCloudDirector.psm1
+++ CCL.VMware.vCloudDirector/CCL.VMware.vCloudDirector.psm1
@@ -1,12 +1,3 @@
-###############################################################
-# Avoid putting any additional logic in this PSM1 because if
-# it the module is compiled into a monolithic PSM1, this file
-# is created from scratch.
-#
-# Instead, initialization logic should go into Initialize.ps1.
-# That file will be compiled into the PSM1 if that option is
-# enabled in $PSBPreference.Build.CompileModule
-###############################################################
 # Dot source public/private functions
 $public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
 $private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.0.1] - 2021-05-27
+
+### Changed
+
+- Wait-vCloudDirectorTask - Change error type to stop pipeline execution fail
+- Scaffolding (psakeFile.ps1, requirements.psd1, CCL.VMware.vCloudDirector.psm1) - Updated from Stucco version 0.4.0
+
+
 ## [1.0.0] - 2021-04-06
 
 ### Added
